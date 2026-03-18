ffeac4898458ab8ccd60619e9a18d2796b8828df
Pavel Sturc <psturc@redhat.com>
2026-03-18 17:23:24 +0100

    test

--- may/test/e2e/e2e_suite_test.go
+++ may/test/e2e/e2e_suite_test.go
@@ -72,7 +72,8 @@ func TestE2E(t *testing.T) {
 var _ = BeforeSuite(func() {
 	By("building the manager(Operator) image")
 	imgParam := fmt.Sprintf("IMG=%s", projectImage)
-	cmd := exec.Command("make", "docker-build", imgParam)
+	coverageParam := fmt.Sprintf("ENABLE_COVERAGE=%s", os.Getenv("ENABLE_COVERAGE"))
+	cmd := exec.Command("make", "docker-build", imgParam, coverageParam)
 	_, err := utils.Run(cmd)
 	ExpectWithOffset(1, err).NotTo(HaveOccurred(), "Failed to build the manager(Operator) image")
 
